26ff3e752aa7debfc0c295506845a2a501374d7a
LeivaDiego <lei21752@uvg.edu.gt>
2023-11-22 20:24:20 -0600

    yaml de MT reconocedora

--- Proyecto3/Proyecto3/Proyecto3.py
+++ Proyecto3/Proyecto3/Proyecto3.py
@@ -1,15 +1,15 @@
 from MT import MaquinaTuring
 
 def main():
-	mt = MaquinaTuring()
-	mt.load_from_yaml("testMT.yaml")
+	mtr = MaquinaTuring()
+	mtr.load_from_yaml("Reconocedora.yaml")
 	#mt.debug_print()
-	for input_string in mt.simulation_strings:
+	for input_string in mtr.simulation_strings:
 		print("---------------------------------")
 		print(f"Input: {input_string}")
 		print("---------------------------------")
 		print("Descripciones Instantaneas: ")
-		tape_result, is_accepted = mt.run(input_string)
+		tape_result, is_accepted = mtr.run(input_string)
 		print("---------------------------------")
 		print("Resultado:")
 		if is_accepted:
@@ -17,6 +17,23 @@ def main():
 		else:
 			print("La cadena es rechazada por la MT")
 		print("---------------------------------\n\n")
+	
+	# mta = MaquinaTuring()
+	# mta.load_from_yaml("Alteradora.yaml")
+	# #mt.debug_print()
+	# for input_string in mta.simulation_strings:
+	# 	print("---------------------------------")
+	# 	print(f"Input: {input_string}")
+	# 	print("---------------------------------")
+	# 	print("Descripciones Instantaneas: ")
+	# 	tape_result, is_accepted = mta.run(input_string)
+	# 	print("---------------------------------")
+	# 	print("Resultado:")
+	# 	if is_accepted:
+	# 		print("La cadena es aceptada por la MT")
+	# 	else:
+	# 		print("La cadena es rechazada por la MT")
+	# 	print("---------------------------------\n\n")
 
 if __name__ == "__main__":
 	main()
